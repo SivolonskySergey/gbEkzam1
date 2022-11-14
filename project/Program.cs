33dc9445fe381d9ff059c332381bb427af770010
Sergey <bibert.shmibert@gmail.com>
2022-11-14 22:05:20 +0400

    add logic and few methods

--- project/Program.cs
+++ project/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"'{arr[i]}' ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void fillArray(int length, string[] arrToFill)
+{
+    for (int i = 0; i < length; i++)
+    {
+        arrToFill[i] = Console.ReadLine();
+    }
+}
+
+void lessThan3Char(int length)
+{
+    Console.WriteLine($"Введите {length} слов: ");
+
+    string[] startArr = new string[length];
+
+    fillArray(length, startArr);
+
+    Console.WriteLine("Стартовый массив: ");
+
+    PrintArray(startArr);
+
+    int newLength = 0;
+    for (int i = 0; i < startArr.Length; i++)
+    {
+        if (startArr[i].Length <= 3)
+        {
+            newLength += 1;
+        }
+    }
+    string[] finalArray = new string[newLength];
+    int newIndex = 0;
+    for (int i = 0; i < startArr.Length; i++)
+    {
+        if (startArr[i].Length <= 3)
+        {
+            finalArray[newIndex] = startArr[i];
+            newIndex++;
+        }
+    }
+    Console.WriteLine("Финальный массив: ");
+    PrintArray(finalArray);
+}
+
+
+lessThan3Char(5);
